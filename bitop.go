84f5bf2adb7beaa0731dc1b7aa982f8f96cf1f6a
Yulin Chen <56228115+yulin-physics@users.noreply.github.com>
2022-06-07 17:48:18 +0100

    Replace (#7)

--- bitop.go
+++ bitop.go
@@ -48,32 +48,47 @@ func LastIndexOfZero(b uint) int {
 
 // GetBitAtIndex returns the bit at index `ind` of the given binary, index counting from left to right from zero as usual
 // `Leng` is an optional argument to specify the exact length of the binary including leading zeroes
-func GetBitAtIndex(b uint, ind uint, leng int) uint {
+func GetBitAtIndex(b uint, ind int, leng int) uint {
 	if leng == -1 {
 		leng = bits.Len(b)
 	}
-	return b >> (uint(leng) - ind - 1) & 1
+	return b >> uint(leng-ind-1) & 1
 }
 
-// SplitAt returns the binary argument in two halves at the index specified [0, index), excluding leading zero bits
-func SplitAt(b uint, index int) []uint {
-	firstHalf := uint(0) | b>>(bits.Len(b)-index)
-	return []uint{firstHalf, b ^ (firstHalf << (bits.Len(b) - index))}
+// SplitAt returns the binary argument in two halves at the index specified [0, index), excluding leading zero bits if leng is -1
+func SplitAt(b uint, index int, leng int) []uint {
+	if leng < 0 {
+		leng = bits.Len(b)
+	}
+	firstHalf := uint(0) | b>>(leng-index)
+	return []uint{firstHalf, b ^ (firstHalf << (leng - index))}
 }
 
 // TruncateFromRight returns the binary truncated up to the index from the right, exclusive of the index
-func TruncateFromRight(b uint, index int) uint {
-	return b >> index
+func TruncateFromRight(b uint, pos int) uint {
+	if pos < 0 {
+		return b
+	}
+	return b >> pos
 }
 
 // ClearFromRight returns the binary bits set to zero up to the index from the right, exclusive of the index
 func ClearFromRight(b uint, index int) uint {
+	if index < 0 {
+		return b
+	}
 	return b &^ (1<<index - 1)
 }
 
-// TruncateFromLeft returns binary truncated up to the index from the left, exclusive of the index
-func TruncateFromLeft(b uint, index int) uint {
-	return b &^ ((1<<index - 1) << (bits.Len(b) - index))
+// TruncateFromLeft returns binary truncated up to the index from the left, exclusive of the index; excluding leading zero bits if leng is -1
+func TruncateFromLeft(b uint, index int, leng int) uint {
+	if index < 0 {
+		return b
+	}
+	if leng < 0 {
+		leng = bits.Len(b)
+	}
+	return b &^ ((1<<index - 1) << (leng - index))
 }
 
 // RemoveBit returns the binary with the bit at index removed, length of the binary decreases by one
@@ -113,8 +128,11 @@ func ColumnJoin(rows []uint, colLen int) []uint {
 	return cols
 }
 
-// Repeat returns a binary that is a repetition of the given bit pattern `b`, with length of the repeated unit `leng` (since leading zeroes would be ignored otherwise) and number of repetitions `count`
-func Repeat(b uint, leng int, count int) uint {
+// Repeat returns a binary that is a repetition of the given bit pattern `b`, with length of the repeated unit `leng` (since leading zeroes would be ignored otherwise, parse -1 for leng if length unknown) and number of repetitions `count`
+func Repeat(b uint, count int, leng int) uint {
+	if leng < 0 {
+		leng = bits.Len(b)
+	}
 	combined := uint(0)
 	for i := 0; i < count; i++ {
 		combined = combined<<leng | b
@@ -122,3 +140,30 @@ func Repeat(b uint, leng int, count int) uint {
 	return combined
 }
 
+// Replace returns a binary with any old bit pattern replaced by new, up to n times of occurrences
+// leng, oldLeng, newLeng are optional properties for b, old, new, to account for leading zeroes
+func Replace(b uint, old uint, new uint, n int, leng int, oldLeng int, newLeng int) uint {
+	if leng < 0 {
+		leng = bits.Len(b)
+	}
+	if oldLeng < 0 {
+		oldLeng = bits.Len(old)
+	}
+	if newLeng < 0 {
+		newLeng = bits.Len(new)
+	}
+	result := uint(0)
+	for i := 0; i < leng; {
+		window := TruncateFromLeft(b, i, leng)
+		window = TruncateFromRight(window, leng-i-oldLeng)
+		if window == old && n > 0 {
+			result = result<<newLeng | new
+			n--
+			i += oldLeng
+		} else {
+			result = result<<1 | GetBitAtIndex(b, i, leng)
+			i++
+		}
+	}
+	return result
+}
--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,6 @@ import (
 )
 
 func main() {
-	a := bitop.ColumnJoin([]uint{0b001101, 0b11111}, 6)
-
+	a := bitop.Replace(0b101010, 0b01, 0b1, 2, 6, 2, 1)
 	fmt.Printf("%02b", a)
 }
